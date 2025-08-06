f772a8ffd6a753b18d99662e1f6576db2223bad3
Sai Manasa Karanam <saimanasakaranam@gmail.com>
2025-08-06 19:53:16 +0530

    Trigger CI workflow

--- app/main.py
+++ app/main.py
@@ -6,7 +6,3 @@ app = FastAPI()
 @app.get("/hello")
 def read_root():
     return {"message": "Hello, DevOps World!"}
-
-@app.get("/favicon.ico")
-async def favicon():
-    return FileResponse("images\favicon.ico")
